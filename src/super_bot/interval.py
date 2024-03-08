ac6a182d6c0f2e00413eca89f0caea6796af6f56
Filip Strajnar <filip.strajnar@gmail.com>
2024-03-08 21:15:43 +0100

    Added interval class.

---
+++ src/super_bot/interval.py
@@ -0,0 +1,14 @@
+from datetime import timedelta, datetime
+
+
+class Interval:
+
+    def __init__(self, repeat_interval: timedelta, action: callable[[], None]):
+        self.repeat_interval = repeat_interval
+        self.action = action
+        self.last_used = datetime.now() - repeat_interval
+
+    def check_and_execute(self):
+        if (self.last_used + self.repeat_interval) < datetime.now():
+            self.action()
+            self.last_used = datetime.now()
